41be90617e7c50c05a80314597eedf3199c31169
Andriy Polyanskiy <71780594+ppolyanskiyy@users.noreply.github.com>
2020-10-07 16:38:31 +0300

    Update Main.c

--- Main.c
+++ Main.c
@@ -1,4 +1,4 @@
-#include"List.h"
+#include"list.h"
 
 int main() {
 
@@ -8,10 +8,10 @@ int main() {
 	StringListPrint(head_node);
 
 	StringListInit(&head_node, "Second");
-	
-	StringListAdd(head_node, "hello1");
+
+	StringListAdd(head_node, "hello");	
 	StringListAdd(head_node, "hello2");
-	StringListAdd(head_node, "hello3");
+	StringListAdd(head_node, "hello");
 	StringListAdd(head_node, "world");
 	StringListAdd(head_node, "Last");
 
@@ -38,12 +38,39 @@ int main() {
 	StringListRemove(&head_node, "Last");
 	StringListPrint(head_node);
 
-	/*printf("---> Replace <ll> to <ffff> :   ");
-	StringListReplaceInStrings(&head_node, "ll", "ffff");
-	StringListPrint(head_node);	*/
+	printf("---> Replace <hello> to <bye> :   ");
+	StringListReplaceInStrings(head_node, "hello", "bye");
+	StringListPrint(head_node);	
 
+	printf("---> Destroy List :   ");
 	StringListDestroy(&head_node);
 	StringListPrint(head_node);
+
+	printf("\n");
+	char** head_node2 = NULL;
+
+	StringListInit(&head_node2, "b");
+	StringListAdd(head_node2, "d");
+	StringListAdd(head_node2, "a");
+	StringListAdd(head_node2, "b");
+	StringListAdd(head_node2, "c");
+	StringListAdd(head_node2, "b");
+	StringListAdd(head_node2, "e");
+	StringListAdd(head_node2, "c");
+
+	StringListPrint(head_node2);
+
+	printf("---> Sorting :   ");
+	StringListSort(&head_node2);
+	StringListPrint(head_node2);
+
+	printf("---> Remove Duplicates :   ");
+	StringListRemoveDuplicates(head_node2);
+	StringListPrint(head_node2);
+
+	printf("---> Destroy List :   ");
+	StringListDestroy(&head_node2);
+	StringListPrint(head_node2);
 
 	return 0;
 }
